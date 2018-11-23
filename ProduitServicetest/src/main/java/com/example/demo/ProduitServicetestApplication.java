e1d2d8aba6966ca0323d8f3f79714d06f5663097
messadi <messadi1994@gmail.com>
2018-11-24 00:53:38 +0100

    y

---
+++ ProduitServicetest/src/main/java/com/example/demo/ProduitServicetestApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProduitServicetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProduitServicetestApplication.class, args);
+	
+	
+	}
+}
